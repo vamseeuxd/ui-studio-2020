f696b517beb5f985f8e3ea2f13cc9556a1aef934
Vamsee Kalyan <vamsee.uxd@gmail.com>
2021-08-31 06:59:48 +0530

    Work in Progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
   componentToEdit: IComponent | null = null;
   // Institutional Trade Processing
   app: IApplication = {
-    name: 'Institutional Trade Processing',
+    name: '',
     layout: 'blank-layout',
     defaultPage: 'page_140',
     pages: [
